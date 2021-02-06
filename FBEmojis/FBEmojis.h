35e0d967ea2b67cc937001b70479a08db48dcbeb
rajeev-rj07 <rajeev.kulariya1996@gmail.com>
2021-02-06 17:54:08 +0530

    Initial commit

---
+++ FBEmojis/FBEmojis.h
@@ -0,0 +1,18 @@
+//
+//  FBEmojis.h
+//  FBEmojis
+//
+//  Created by Rajeev Kulariya on 06/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FBEmojis.
+FOUNDATION_EXPORT double FBEmojisVersionNumber;
+
+//! Project version string for FBEmojis.
+FOUNDATION_EXPORT const unsigned char FBEmojisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBEmojis/PublicHeader.h>
+
+
