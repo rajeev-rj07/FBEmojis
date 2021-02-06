616e042498a3621945966d3d8ab5330b3b461283
rajeev-rj07 <rajeev.kulariya1996@gmail.com>
2021-02-06 18:46:14 +0530

    Add sample test

---
+++ FBEmojis/FBEmojis.swift
@@ -0,0 +1,20 @@
+//
+//  FBEmojis.swift
+//  FBEmojis
+//
+//  Created by Rajeev Kulariya on 06/02/21.
+//
+
+public final class FBEmojis {
+
+    let name = "FBEmojis"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
